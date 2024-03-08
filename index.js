c4f8cfb21fc52777c73486a67e82fe4abb92e50c
Enrico Zappalà <zpp.enrico@gmail.com>
2024-03-08 08:07:00 +0100

    Add initial code to main file

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 
-console.log(process.env.DISCORD_TOKEN, process.env.TEST_SERVER_TOKEN);
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const TEST_SERVER_ID = process.env.TEST_SERVER_ID;
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord using token
+client.login(DISCORD_TOKEN);
